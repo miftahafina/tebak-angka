bd9e051a1b1a6e92083e6dc74354b67d7d90eb97
Miftah Afina <miftah.afina@gmail.com>
2016-02-10 21:29:07 +0700

    Generate nomor 1-100

--- tebak.rb
+++ tebak.rb
@@ -4,9 +4,15 @@
 
 # Memasukkan nama
 puts "Selamat datang di permainan Tebak Angka"
-puts "Nama anda: "
+puts "Nama Anda: "
 
 input_nama = gets
 nama = input_nama.chomp
 
 puts "Selamat bermain, #{nama}!"
+
+# Generate nomor
+puts "Saya memiliki angka antara 1 - 100."
+puts "Dapatkah Anda menebaknya?"
+target = rand(100) + 1
+puts "(kunci: #{target})"
