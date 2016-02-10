ca406b7a87a543699e39572ed39078e5f396076f
Miftah Afina <miftah.afina@gmail.com>
2016-02-10 21:24:34 +0700

    Ambil nama pemain

---
+++ tebak.rb
@@ -0,0 +1,12 @@
+# Permainan Tebak Angka
+# Oleh: Miftah Afina
+# Dipelajari dari: Head First Ruby (O'reilly)
+
+# Memasukkan nama
+puts "Selamat datang di permainan Tebak Angka"
+puts "Nama anda: "
+
+input_nama = gets
+nama = input_nama.chomp
+
+puts "Selamat bermain, #{nama}!"
